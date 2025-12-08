6e6ab331376d24e6caf96b8e890c5fca51763949
Valerie Attila Al-fath <akunvalerie76@gmail.com>
2025-12-08 03:41:11 +0000

    project setup part2

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div className="">
+			<Button variant={'default'} size={"default"}>Click Me</Button>
+		</div>
+	);
+} 
