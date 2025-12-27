1b691def6919ff79445eb846f74d78bc1682780b
No one knows <lnvvalerie60@gmail.com>
2025-12-27 15:40:31 +0000

    resolving high level risk

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://b2e1e966f9bfc6590058b4621584ba86@o4510330183942144.ingest.de.sentry.io/4510607477375056",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
